a5267c306c4fae8f13f67c484c5fab26f836241e
felipedelosh <doctorfhernandez@hotmail.com>
2024-10-20 15:37:25 -0500

    save data Pretty & Order

--- main.py
+++ main.py
@@ -34,6 +34,8 @@ def cleanID(id):
 def cleanData(data):
     data = str(data).lstrip().rstrip()
     data = data.replace(".", "")
+    data = data.replace("\n", "")
+    data = re.sub(r'\u200B', '', data)
 
     return data
 
@@ -43,8 +45,14 @@ def orderDATA(arrData):
     Enter a information in arr of tuples [(id, info), ...(id, info)]
     return the array in ASC order
     """
-    for i in range(0, len(arrData)):
-        print(arrData[i])
+    n = len(arrData)
+    for i in range(0, n):
+        for j in range(0, n - i - 1):
+            if int(arrData[j][0]) > int(arrData[j+1][0]):
+                temp = arrData[j]
+                arrData[j] = arrData[j+1]
+                arrData[j+1] = temp
+
 
 data1000WayToLie = []
 temp = ""
@@ -67,4 +75,11 @@ print("================STEP 03 of 03 - SAVE DATA======================")
 with open("originalDATA.csv", "w", encoding="UTF-8") as f:
     f.write(temp)
 
-#data1000WayToLie = orderDATA(data1000WayToLie)
+
+_dataOrder100waysToLie = "ID|DEATH\n"
+orderDATA(data1000WayToLie)
+for i in data1000WayToLie:
+    _dataOrder100waysToLie = _dataOrder100waysToLie + f"{i[0]}|{i[1]}\n"
+
+with open("1000WaysToLie.csv", "w", encoding="UTF-8") as f:
+    f.write(_dataOrder100waysToLie)
